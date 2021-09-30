e9c50361948edef894d73ed10407c00ffb762d90
Csmeau4 <57441678+Csmeau4@users.noreply.github.com>
2021-09-30 16:38:15 +0200

    envoi de mails

--- src/sendgrid/sendgrid.service.ts
+++ src/sendgrid/sendgrid.service.ts
@@ -2,19 +2,18 @@ import * as sgMail from '@sendgrid/mail';
 import { sendToEmployee } from '../campaign/campaign.service';
 import config from '../config';
 
-export const EMAIL = 'google@epsiwis.fr';
-export const CONTENT = 
-'<p>Alerte de sécurité sur votre compte google</p><br><a href="google.fr">Connectez vous</a> pour la consulter';
+export const EMAIL = 'noreply.goog1e@epsiwis.fr';
 
 
 export const sendMail = async (email) => {
     sgMail.setApiKey(config.server.sendgridKey);
     
+    const CONTENT = '<p style="color:red; font-size: 24px;"><strong>Tentative de connexion bloquée</strong></p><br>Quelqu\'un vient d\'utiliser votre mot de passe pour se connecter à votre compte. La tentative a été rejetée, mais nous vous conseillons de <a href="localhost:3000/google">vous connecter</a> pour vérifier ce qui s\'est passé.'
 
     const msg = {
         to: email,
         from: EMAIL,
-        subject: 'C\'est très la russie, gare à tes données',
+        subject: 'Alerte de sécurité Gmail',
         html: CONTENT,
     }
 
